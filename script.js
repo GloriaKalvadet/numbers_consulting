6e0780a5c125a2444032fbb392b0dbb724f78b83
Gloria Geraldine Kalvadet <geraldinekalvadet27@gmail.com>
2023-04-03 20:06:25 +0300

    updated

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const menuToggle = document.querySelector('.menu-toggle');
-  const menuMobile = document.querySelector('.menu-mobile');
-  menuToggle.addEventListener('click', function() {
-    menuMobile.classList.toggle('active');
-  });
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
